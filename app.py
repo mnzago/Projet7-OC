e38de0c1fbe7e2998b64514ce6dee027a4fefc09
merry <meri_zago@hotmail.fr>
2023-03-16 08:10:56 +0100

    ajout interpretation

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pickle
 from lightgbm import LGBMClassifier
 from sklearn.preprocessing import StandardScaler, RobustScaler
+import shap
 
 app = Flask(__name__)
 
@@ -15,6 +16,21 @@ df = load_pickle('data_cleaned_sample')
 df = df.set_index('SK_ID_CURR')
 df_cli = df[[c for c in df.columns if c not in ['TARGET']]]
 list_clients = list(df_cli.index)
+
+# Pour la prédiction d'un client
+
+pipeline = load_pickle('model')
+scaler = StandardScaler()
+scaler_df = pd.DataFrame(scaler.fit_transform(df_cli), 
+                        columns=df_cli.columns,
+                        index=df_cli.index)
+model = pipeline.steps[1][1]
+
+threshold = 0.7
+
+#Pour l'interprétation
+explain_shap = load_pickle('explainer')
+nb_client_explain_global = 1000
 
 @app.route('/')
 @app.route('/clients/',  methods=['GET'])
@@ -29,17 +45,7 @@ def client(id):
     response = data_client.to_dict(orient='index')
     return jsonify(response)
 
-# Pour la prédiction d'un client
-
-pipeline = load_pickle('model')
-scaler = StandardScaler()
-scaler_df = pd.DataFrame(scaler.fit_transform(df_cli), 
-                        columns=df_cli.columns,
-                        index=df_cli.index)
-model = pipeline.steps[1][1]
-
-threshold = 0.7
-
+# Pour la prédiction
 @app.route('/predict/<id>',  methods=['GET'])
 def predict(id):
     data_cli = scaler_df[scaler_df.index==int(id)]
@@ -51,6 +57,30 @@ def predict(id):
         "probability": y_prob}
     return jsonify(response)
 
+#Interprétation locale
+@app.route('/explain/<id>',  methods=['GET'])
+def explain_id(id):
+    data_cli = scaler_df[scaler_df.index==int(id)]
+    shap_values = explain_shap.shap_values(data_cli, check_additivity=False).tolist()
+    expected_value = explain_shap.expected_value
+    response = {"id_": id,
+                "explain_data_id": data_cli.to_dict(),  # renvoie les données pour l'interprétabilité locale
+                "shap_values": shap_values, # renvoie les valeurs de Shapely
+                "expected_value":  expected_value # renvoie la valeur de base
+            }
+    return jsonify(response)
 
+#Interprétation globale
+@app.route('/explain/',  methods=['GET'])
+def explain_all():
+    data_1000_cli = scaler_df.sample(n=nb_client_explain_global, random_state=42)
+    shap_values = explain_shap.shap_values(data_1000_cli, check_additivity=False).tolist()
+    expected_value = explain_shap.expected_value
+    response = {"explain_data": data_1000_cli.to_dict(),  # renvoie les données pour l'interprétabilité locale
+                "shap_values": shap_values, # renvoie les valeurs de Shapely
+                "expected_value": expected_value  # renvoie la valeur de base
+            }
+    return jsonify(response)
+    
 if __name__ == '__main__':
     app.run()
--- app_streamlit.py
+++ app_streamlit.py
@@ -1,9 +1,15 @@
-import requests
 import streamlit as st
-from PIL import Image
-import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
+import requests
+from PIL import Image
+import plotly.graph_objects as go
+import plotly.express as px
+from sklearn.preprocessing import StandardScaler, RobustScaler
+from lightgbm import LGBMClassifier
+import shap
+from streamlit_shap import st_shap
+import matplotlib.pyplot as plt
 
 URL = "https://test-projet-v0.herokuapp.com"
 
@@ -43,6 +49,7 @@ def display_customer_data(element):
     st.subheader(f"Voici les données et les résultats du client {element}")
     st.write(pd.DataFrame.from_dict(r, orient='index'))
 
+# On obtient la gauge pour visualiser le refus ou l'accord du prêt
 def gauge_plot(probability, threshold):
     value = round(probability, 2) * 100
     percent = 0.1
@@ -87,6 +94,90 @@ def prediction_cli(element):
     gauge = gauge_plot(data_result[0]['probability'], data_result[0]['threshold'])
     st.write(gauge)
 
+# On récupère les résultats pour afficher l'interprétation globale et locale
+
+ # Interprétation globale
+@st.cache_data(show_spinner=False)
+def get_explain_from_all():
+    response = requests.get(f"{API_URL}/explain/")
+    return response.json()
+
+def explain_global():
+    result = get_explain_from_all()
+    data = pd.DataFrame.from_dict(result['explain_data'], orient='index').T
+    shap_values = pd.DataFrame(result['shap_values'])
+    st.markdown(
+        """<style>
+        .streamlit-expanderContent {background:white;}
+        </style>""",
+        unsafe_allow_html=True,
+    )  # Un peu de style CSS pour garantir un fond blanc sur les expander (graphiques SHAP mieux intégrés !)
+    st_shap(shap.summary_plot(np.array(shap_values), data, plot_type="bar", max_display = 10))
+    st.markdown(
+        """<style>
+        .streamlit-expanderContent {background:white;}
+        </style>""",
+        unsafe_allow_html=True,
+    )
+    st_shap(shap.summary_plot(np.array(shap_values), data, plot_type="violin", max_display = 10))
+
+# Interprétation locale
+@st.cache_data(show_spinner=False)
+def get_explain_from_customer(id):
+    response = requests.get(f"{API_URL}/explain/{id}")
+    return response.json()
+
+def explain_local(element):
+    result = get_explain_from_customer(element)
+    data_cli = pd.DataFrame.from_dict(result['explain_data_id'], orient='index').T
+    shap_values = pd.DataFrame(result['shap_values'])
+    expected_value = result['expected_value']
+    st.write('data_cli:', data_cli, 
+            'shap_values:', shap_values,
+            'expected_value:' , expected_value)
+
+    interp = shap.Explanation(np.array(shap_values)[0],
+                        base_values=expected_value,
+                        feature_names=data_cli.columns,
+                        data=data_cli.values[0])
+    st.markdown(
+        """<style>
+        .streamlit-expanderContent {background:white;}
+        </style>""",
+        unsafe_allow_html=True,
+    )  # Un peu de style CSS pour garantir un fond blanc sur les expander (graphiques SHAP mieux intégrés !)
+    fig3, ax = plt.subplots()
+    plt.title("Interprétation locale")
+    ax = shap.plots.waterfall(interp)
+    st.pyplot(fig3)
+
+# Situation du client par rapport à un échantillon de 1000 clients
+def feature_boxplot(data, feature, value, customer_id):
+    fig = px.box(data, x=feature)
+    fig.add_vline(x=value,
+                  line_color="rgb(32,32, 32)",
+                  annotation_text=f'Client: {customer_id}'
+                  )
+    return fig
+
+def display_interp_feature(element):
+    result_global = get_explain_from_all()
+    data = pd.DataFrame.from_dict(result_global['explain_data'], orient='index').T
+
+    result_local = get_explain_from_customer(element)
+    data_cli = pd.DataFrame.from_dict(result_local['explain_data_id'], orient='index').T
+
+    features = pd.DataFrame.from_dict(data).columns.tolist()
+    variable = st.sidebar.selectbox("Choisir une variable", features,
+                     help='Visualiser le positionnement du client par rapport à cette variable.') 
+    fig = feature_boxplot(data=data,
+                        feature=variable,
+                        value=data_cli[variable].values[0],
+                        customer_id=element)
+    st.plotly_chart(figure_or_data=fig, use_container_width=True)
+
+#---- Fonction main     
+
 client = st.sidebar.text_input(label="Saisir l'identifiant d'un client :bust_in_silhouette:")
 list_cli = get_list_clients()
 if client == '':
@@ -96,3 +187,6 @@ elif int(client) not in list_cli:
 else:
     display_customer_data(client)
     prediction_cli(client)
+    explain_local(client)
+    explain_global()
+    display_interp_feature(client)
